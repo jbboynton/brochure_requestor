8c16ce37971ef5e7ce03f166f69549c5efd0d2ef
James Boynton <james@xzito.com>
2018-09-18 23:02:25 -0400

    Disable submit until AJAX completes

--- brochure_requestor.php
+++ brochure_requestor.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Plugin Name:		Brochure Requestor
- * Description:		Insert a brochure request form via a shortcode.
- * Version:			0.1
- * Author:			James Boynton
- * Author URI:		#
- * Text Domain:		br
+ * Plugin Name:    Brochure Requestor
+ * Description:    Insert a brochure request form via a shortcode.
+ * Version:        0.2
+ * Author:         James Boynton
+ * Author URI:     #
+ * Text Domain:    br
  */
 
 defined ('ABSPATH') or die('Direct access not permitted.');
 
 function br_brochure_requestor_init() {
   define ('BROCHURES_PAGE', '4503');
-  
+
   if ($_POST['brochure_request_submitted']) {
     if (br_verify_nonce()) {
-	  $args = br_build_message_fields();
+      $args = br_build_message_fields();
       $content = br_generate_message_content($args);
 
       br_send_pending_mail($content);
@@ -29,6 +29,7 @@ function br_build_message_fields() {
   $title          = (isset($request['title'])) ? $request['title'] : '';
   $name           = (isset($request['name'])) ? $request['name'] : '';
   $email          = (isset($request['email'])) ? $request['email'] : '';
+  $company        = (isset($request['company'])) ? $request['company'] : '';
   $address_line_1 = (isset($request['address_line_1'])) ? $request['address_line_1'] : '';
   $address_line_2 = (isset($request['address_line_2'])) ? $request['address_line_2'] : '';
   $city           = (isset($request['city'])) ? $request['city'] : '';
@@ -38,25 +39,27 @@ function br_build_message_fields() {
 
   $fields = array(
     'title' => $title,
-	'name' => $name,
-	'email' => $email,
-	'address_line_1' => $address_line_1,
-	'address_line_2' => $address_line_2,
-	'city' => $city,
-	'state' => $state,
-	'zip' => $zip,
-	'country' => $country
+    'name' => $name,
+    'email' => $email,
+    'company' => $company,
+    'address_line_1' => $address_line_1,
+    'address_line_2' => $address_line_2,
+    'city' => $city,
+    'state' => $state,
+    'zip' => $zip,
+    'country' => $country
   );
 
   return $fields;
 }
 
 function br_send_pending_mail($body) {
-  $recipient = 'production@xzito.com';
+  $recipient = 'dev@xzito.com,info@hornermillwork.com';
   $subject = 'New Brochure Request';
 
   wp_mail($recipient, $subject, $body);
   wp_safe_redirect($_SERVER['REQUEST_URI']);
+
   die();
 }
 
@@ -67,6 +70,7 @@ function br_generate_message_content($fields) {
   $message .= "Brochure:         " .$fields["title"] ."\n";
   $message .= "For:              " .$fields["name"] ."\n";
   $message .= "Email:            " .$fields["email"] ."\n";
+  $message .= "Company:          " .$fields["company"] ."\n";
   $message .= "Address Line 1:   " .$fields["address_line_1"] ."\n";
   $message .= "Address Line 2:   " .$fields["address_line_2"] ."\n";
   $message .= "City:             " .$fields["city"] ."\n";
@@ -81,21 +85,21 @@ function br_generate_message_content($fields) {
 function br_enqueue_assets() {
   if (is_page(BROCHURES_PAGE)) {
 
-	// Bootstrap
+    // Bootstrap
     wp_enqueue_script('br_bootstrap_script', plugin_dir_url(__FILE__) .
-	  'vendor/bootstrap/bootstrap.min.js', array('jquery'));
+      'vendor/bootstrap/bootstrap.min.js', array('jquery'));
     wp_enqueue_style('br_bootstrap_style', plugin_dir_url(__FILE__) .
-	  'vendor/bootstrap/bootstrap.min.css');
+      'vendor/bootstrap/bootstrap.min.css');
 
     // Plugin style
-	wp_enqueue_style('br_request_form_style', plugin_dir_url(__FILE__) .
-	  'css/request_form.css');
+    wp_enqueue_style('br_request_form_style', plugin_dir_url(__FILE__) .
+      'css/request_form.css');
 
     // Form submission script
-	wp_enqueue_script('br_form_helper_script', plugin_dir_url(__FILE__) .
-	  'js/form_helper.js', array('jquery'));
+    wp_enqueue_script('br_form_helper_script', plugin_dir_url(__FILE__) .
+      'js/form_helper.js', array('jquery'));
     wp_localize_script('br_form_helper_script', 'br_requested_brochure', array(
-	  'ajax_url' => admin_url('admin-ajax.php')
+      'ajax_url' => admin_url('admin-ajax.php')
     ));
   }
 }
@@ -126,11 +130,18 @@ function br_get_requested_brochure_data() {
 
   die();
 }
-add_action('wp_ajax_nopriv_br_get_requested_brochure_data', 'br_get_requested_brochure_data');
-add_action('wp_ajax_br_get_requested_brochure_data', 'br_get_requested_brochure_data');
+add_action(
+  'wp_ajax_nopriv_br_get_requested_brochure_data',
+  'br_get_requested_brochure_data'
+);
+add_action(
+  'wp_ajax_br_get_requested_brochure_data',
+  'br_get_requested_brochure_data'
+);
 
 function br_incremented_id() {
   global $post;
+
   $counter = get_post_meta($post->ID, 'br_counter', true);
   $counter = isset($counter) ? $counter : 1;
 
@@ -139,6 +150,7 @@ function br_incremented_id() {
 
 function updateCounter($current_id) {
   global $post;
+
   update_post_meta($post->ID, 'br_counter', ++$current_id);
 }
 
@@ -152,23 +164,23 @@ function br_request_brochure_shortcode() {
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <form class="form-horizontal" action="<?php the_permalink(); ?>" method="post">
-			<?php wp_nonce_field(basename(__FILE__), 'br_request_brochure_nonce'); ?>
+            <?php wp_nonce_field(basename(__FILE__), 'br_request_brochure_nonce'); ?>
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               <h4 class="modal-title">Receive a Brochure By Mail</h4>
             </div>
             <div class="modal-body">
-			  <div class="form-group">
-				<div class="col-sm-12">
-			      <p class="form-control-static brochure-title" id="requested-brochure-name-<?php echo br_incremented_id(); ?>"></p>
+              <div class="form-group">
+                <div class="col-sm-12">
+                  <p class="form-control-static brochure-title" id="requested-brochure-name-<?php echo br_incremented_id(); ?>"></p>
                   <input hidden type="text" id="requested-brochure-title-<?php echo br_incremented_id(); ?>" name="brochure_request[title]" value="">
-				</div>
-			  </div>
-			  <div class="form-group">
-				<div class="col-sm-12">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-12">
                   <div class="brochure-image" id="requested-brochure-image-<?php echo br_incremented_id(); ?>" name="brochure_request[image_url]"></div>
-				</div>
-			  </div>
+                </div>
+              </div>
               <div class="form-group">
                 <label for="brochure_request[name]" class="col-sm-3 control-label">Name</label>
                 <div class="col-sm-9">
@@ -179,6 +191,12 @@ function br_request_brochure_shortcode() {
                 <label for="brochure_request[email]" class="col-sm-3 control-label">Email Address</label>
                 <div class="col-sm-9">
                   <input type="email" class="form-control" id="email" name="brochure_request[email]" placeholder="Email Address">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="brochure_request[company]" class="col-sm-3 control-label">Company</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" id="company" name="brochure_request[company]" placeholder="Company Name">
                 </div>
               </div>
               <div class="form-group">
@@ -218,10 +236,10 @@ function br_request_brochure_shortcode() {
                 </div>
               </div>
             </div>
-			<input hidden type="text" name="brochure_request_submitted" value="1">
+            <input hidden type="text" name="brochure_request_submitted" value="1">
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">Request Brochure</button>
+              <button type="submit" class="btn btn-primary submitRequest" id="requested-brochure-submit-<?php echo br_incremented_id(); ?>" disabled>Request Brochure</button>
             </div>
           </form>
         </div>
--- js/form_helper.js
+++ js/form_helper.js
@@ -5,59 +5,16 @@
 
 (function($) {
 
-  $(document).ready(function() {
-    console.log('form_helper.js initialized');
-
-  });
-
-  // Uses the reference element to find the brochure title (which does not have
-  // a unique selector).
-  function findBrochureHeader(referenceElement) {
-	var referenceElement = $('#' + referenceElement);
-    var parentDiv = referenceElement.parent();
-	var siblingOfParent = parentDiv.next();
-	var headerDescendant = siblingOfParent.find('h6');
-    return headerDescendant;
-  }
-
-  // Returns the 'h6' node that contains the title and image of the requested
-  // brochure.
-  function findBrochureDataInDOM(referenceElement) {
-    var referenceElement = $('#' + referenceElement);
-	var parentHeader = referenceElement.parent();
-	
-	return parentHeader;
-  }
-
-  function findBrochureName(referenceElement) {
-	var headerElement = findBrochureDataInDOM(referenceElement);
-	var titleTextObjectArray = headerElement.find('strong');
-	var titleTextStringArray = [];
-	titleTextObjectArray.each(function(i) {
-	  titleTextStringArray[i] = $(this).text();
-	});
-	var titleTextString = titleTextStringArray.join(': ');
-	
-    return titleTextString;
-  } 
-
-  function findBrochureImageSource(referenceElement) {
-    var headerElement = findBrochureDataInDOM(referenceElement);
-    var imageElement = headerElement.find('img');
-    var imageSource = imageElement.attr('src');
-
-    return imageSource;
-  } 
-
+  // Insert 'request' links beneath each brochure.
   $(window).load(function() {
     insertRequestLinks();
 
-    var requestLinks = $('.request-link');
-    $(requestLinks).on('click', function(e) {
-      console.log('in the func');
-	  var modalTrigger = this.id;
+    // On click, populate the 'request a brochure' form with the title and image
+    // of the requested brochure.
+    $(document).on('click', '.request-link', function() {
+      var modalTrigger = this.id;
       var brochureName = findBrochureName(modalTrigger);
-      var brochureImage = findBrochureImageSource(modalTrigger); 
+      var brochureImage = findBrochureImageSource(modalTrigger);
 
       $.ajax({
         url: br_requested_brochure.ajax_url,
@@ -68,50 +25,114 @@
           brochure_image: brochureImage
         },
         success: function(response) {
-		  updateModal(modalTrigger, response)
-	    }
+          updateModal(modalTrigger, response);
+        }
       });
     });
   });
 
+  // Uses the reference element to find the brochure title (which does not have
+  // a unique selector).
+  function findBrochureHeader(referenceElement) {
+    var referenceElement = $('#' + referenceElement);
+    var parentDiv = referenceElement.parent();
+    var siblingOfParent = parentDiv.next();
+    var headerDescendant = siblingOfParent.find('h6');
+
+    return headerDescendant;
+  }
+
+  // Returns the 'h6' node that contains the title and image of the requested
+  // brochure.
+  function findBrochureDataInDOM(referenceElement) {
+    var referenceElement = $('#' + referenceElement);
+    var parentHeader = referenceElement.parent();
+
+    return parentHeader;
+  }
+
+  function findBrochureName(referenceElement) {
+    var headerElement = findBrochureDataInDOM(referenceElement);
+    var titleTextObjectArray = headerElement.find('strong');
+    var titleTextStringArray = [];
+
+    titleTextObjectArray.each(function(i) {
+      titleTextStringArray[i] = $(this).text();
+    });
+
+    var titleTextString = titleTextStringArray.join(': ');
+
+    return titleTextString;
+  }
+
+  function findBrochureImageSource(referenceElement) {
+    var headerElement = findBrochureDataInDOM(referenceElement);
+    var imageElement = headerElement.find('img');
+    var imageSource = imageElement.attr('src');
+
+    return imageSource;
+  }
+
   function updateModal(modalTrigger, modalData) {
     var data = JSON.parse(modalData);
+
     updateBrochureName(modalTrigger, data.name);
     updateBrochureTitle(modalTrigger, data.name);
     updateBrochureImage(modalTrigger, data.image);
+    updateBrochureSubmit(modalTrigger);
   }
 
   function updateBrochureName(modalTrigger, brochureName) {
-    var correspondingName = modalTrigger.replace('request-brochure-', 
-	  'requested-brochure-name-');
+    var correspondingName = modalTrigger.replace(
+      'request-brochure-',
+      'requested-brochure-name-'
+    );
     var nameElement = document.getElementById(correspondingName);
-	nameElement.innerHTML = brochureName;
+
+    nameElement.innerHTML = brochureName;
   }
 
   function updateBrochureTitle(modalTrigger, brochureName) {
-    var correspondingTitle = modalTrigger.replace('request-brochure-', 
-	  'requested-brochure-title-');
+    var correspondingTitle = modalTrigger.replace(
+      'request-brochure-',
+      'requested-brochure-title-'
+    );
     var titleElement = document.getElementById(correspondingTitle);
-	$(titleElement).val(brochureName);
+
+    $(titleElement).val(brochureName);
   }
 
   function updateBrochureImage(modalTrigger, brochureImage) {
-    var correspondingImage = modalTrigger.replace('request-brochure-', 
-	  'requested-brochure-image-');
+    var correspondingImage = modalTrigger.replace(
+      'request-brochure-',
+      'requested-brochure-image-'
+    );
     var imageElement = document.getElementById(correspondingImage);
-	imageElement.style.backgroundImage = 'url("'+brochureImage+'")';
+
+    imageElement.style.backgroundImage = 'url("'+brochureImage+'")';
+  }
+
+  function updateBrochureSubmit(modalTrigger) {
+    var correspondingSubmitButton = modalTrigger.replace(
+      'request-brochure-',
+      'requested-brochure-submit-'
+    );
+    var submitButton = $('#' + correspondingSubmitButton);
+
+    submitButton.prop('disabled', false);
   }
 
   function insertRequestLinks() {
     var links = $('.request-link');
+
     links.each(function(i) {
       var header = findBrochureHeader(links[i].id);
       var linkToAppendTo = header.find('a').first().next();
-      
       var separator = document.createTextNode(' | ');
+
       linkToAppendTo.after(separator);
       separator.after(links[i]);
     });
   }
-
 })(jQuery);
+
